c7c43c01dcf96da61e1fdca7afecd1f9b0a04227
SunmeruRS <3127723014@qq.com>
2026-03-23 16:33:50 +0800

    feat: Class for 360 degrees servo

---
+++ firmware_src/servo.py
@@ -0,0 +1,25 @@
+from machine import Pin, PWM
+
+class Servo360():
+    def __init__(self, pin:Pin):
+        self.pin = pin
+        self.pwm = PWM(self.pin, freq=50, duty=0)
+        self.no_cw()
+    
+    def no_cw(self):
+        self.pwm.duty(1.5/20*1024)
+    
+    def slow_cw(self):
+        self.pwm.duty(2/20*1024)
+    
+    def fast_cw(self):
+        self.pwm.duty(2.5/20*1024)
+    
+    def fast_ccw(self):
+        self.pwm.duty(0.5/20*1024)
+
+    def slow_ccw(self):
+        self.pwm.duty(1/20*1024)
+    
+    def stand_by(self):
+        self.pwm.deinit()
