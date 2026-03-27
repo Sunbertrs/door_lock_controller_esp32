07f4ef89ddbfe0e1f4b1bb285d6dab20f433e022
Sunbertrs <3127723014@qq.com>
2026-03-28 02:05:25 +0800

    feat: Renew some initials on bluetooth instance

--- firmware_src/bluetooth_daemon.py
+++ firmware_src/bluetooth_daemon.py
@@ -1,6 +1,10 @@
 import bluetooth
 
 class BluetoothInstance:
-    def __init__(self, name):
+    def __init__(self, device_name, led_indicator):
         self.bluetooth_device = bluetooth.BLE()
+        if self.bluetooth_device.active():
+            self.bluetooth_device.active(False)
         self.bluetooth_device.active(True)
+        self.name = device_name
+        self.indicator: LedIndicator = led_indicator
