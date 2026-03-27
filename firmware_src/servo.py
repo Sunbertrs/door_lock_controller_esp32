7164fc37f3eee4cc32c5306878970444060ba0ac
Sunbertrs <3127723014@qq.com>
2026-03-28 01:53:21 +0800

    fix: Duty value to integer

--- firmware_src/servo.py
+++ firmware_src/servo.py
@@ -7,19 +7,19 @@ class Servo360():
         self.no_cw()
     
     def no_cw(self):
-        self.pwm.duty(1.5/20*1024)
+        self.pwm.duty(int(1.5/20)*1024)
     
     def slow_cw(self):
-        self.pwm.duty(2/20*1024)
+        self.pwm.duty((2/20)*1024)
     
     def fast_cw(self):
-        self.pwm.duty(2.5/20*1024)
+        self.pwm.duty(int(2.5/20)*1024)
     
     def fast_ccw(self):
-        self.pwm.duty(0.5/20*1024)
+        self.pwm.duty(int(0.5/20)*1024)
 
     def slow_ccw(self):
-        self.pwm.duty(1/20*1024)
+        self.pwm.duty((1/20)*1024)
     
     def stand_by(self):
         self.pwm.deinit()
