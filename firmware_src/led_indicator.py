93ee608c1b50d0516db46819dc341743178cb43f
Sunbertrs <3127723014@qq.com>
2026-03-28 02:00:41 +0800

    feat: Add behaviour when bluetooth connected or not

--- firmware_src/led_indicator.py
+++ firmware_src/led_indicator.py
@@ -1,5 +1,4 @@
 from machine import Pin, Timer
-import time
 
 class LedIndicator:
     def __init__(self, pin: Pin, timer: Timer):
@@ -7,23 +6,15 @@ class LedIndicator:
         self.timer = timer
         self.stand_by()
 
-    # for _ in range(10):
-    #     for i in range(0, 1023):
-    #         PWM_CONTROLLER.duty(i)
-    #         time.sleep(0.001)
-    #     for i in range(1023, 0, -1):
-    #         PWM_CONTROLLER.duty(i)
-    #         time.sleep(0.001)
-    PWM_CONTROLLER.freq(1)
-    PWM_CONTROLLER.duty(512)
     def stand_by(self):
+        self.timer.init(mode=Timer.PERIODIC, period=1500, callback=lambda _: self._toggle_light())
 
-def active():
-    PWM_CONTROLLER.deinit()
-    LED_PIN.on()
+    def _toggle_light(self):
+        self.pin.toggle()
 
-    pass
     def in_use(self):
+        self.timer.deinit()
+        self.pin.value(0)
 
     def error(self):
         pass
